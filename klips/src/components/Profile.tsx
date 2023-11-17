44646ec7fac2da9b18b5109a57f7d6bb1ee6a801
Jason D'Souza <jasonaldrindsouza@gmail.com>
2023-11-17 18:57:26 -0500

    added more components

---
+++ klips/src/components/Profile.tsx
@@ -0,0 +1,12 @@
+import { Avatar } from "@mui/material"
+
+const Profile = () => {
+    return (
+        <>
+            <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+        
+        </>
+    )
+}
+
+export default Profile
