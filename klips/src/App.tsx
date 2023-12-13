de4c24e27920fba8ba639f784d70831d7e29ab3f
Jason D'Souza <jasonaldrindsouza@gmail.com>
2023-12-13 17:13:10 -0500

    added video cards to the profile page

--- klips/src/App.tsx
+++ klips/src/App.tsx
@@ -9,12 +9,6 @@ function App() {
       <Box maxWidth= 'xl' sx={{ width: '100%' }}>
         <Header />
         <Profile />
-        <video width={800} height={500} controls loop autoPlay muted>
-            <source 
-              src="https://d28qrcxpk14omv.cloudfront.net/League%20of%20Legends%202022.11.17%20-%2023.15.11.09.mp4"
-              type="video/mp4"
-            />
-        </video>
       </Box>
       
     </>
--- klips/src/components/Profile.tsx
+++ klips/src/components/Profile.tsx
@@ -1,9 +1,8 @@
 import { Avatar, Box, Container, Grid, Toolbar, Typography } from "@mui/material"
 import AdbIcon from '@mui/icons-material/Adb';
 
-const Profile = () => {
-
-    return (        
+const ProfileHeader = () => {
+    return (
         <Container maxWidth='xl'>   
             <Grid container spacing={6} sx={{  }}>
                 <Grid item >
@@ -13,16 +12,14 @@ const Profile = () => {
                     {/* <Box sx={{ display: 'flex', justifyContent: 'center', flexDirection: 'column'}}>  */}
                         <Typography 
                             variant="h1"
-                            sx={{
-                                display: { xs: "none", md: "flex" },
+                            sx={{                   
                                 fontWeight: 700,
                                 color: "#EA3F3F",
-                                textDecoration: "none",
                             }}
                         >
                                 UrineBigTrouble
                         </Typography>
-                        <Box sx={{ display: 'inline-flex', paddingX: 1 }}>
+                        <Box columnGap={0.5} sx={{ display: 'inline-flex', paddingX: 1 }}>
                             <Typography 
                                 sx={{
                                     color: "#EA3F3F",
@@ -38,6 +35,39 @@ const Profile = () => {
 
             </Grid>
         </Container>   
+    )
+}
+
+const VideoCard = () => {
+    return (
+        <Box sx = {{m: 1}}>
+            <video width={480} height={300} controls loop muted>
+            <source 
+              src="https://d28qrcxpk14omv.cloudfront.net/League%20of%20Legends%202022.11.17%20-%2023.15.11.09.mp4"
+              type="video/mp4"
+            />
+             </video>
+        </Box>
+    )
+}
+
+const ProfileContent = () => {
+    return (
+        <Grid container justifyContent="space-evenly" sx={{ mt: 8, mb: 4, borderTop: 1, borderColor: '#8E8E8E'}}>
+            {[...Array(9)].map((_, index) => (
+                <VideoCard key={index} />
+            ))}
+        </Grid>
+    )
+}
+
+const Profile = () => {
+
+    return ( 
+        <>
+            <ProfileHeader />
+            <ProfileContent />
+        </>       
     )
 }
 
